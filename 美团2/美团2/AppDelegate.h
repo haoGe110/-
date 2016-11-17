bf263cdf671dcc7cc773e82951d4869f9354e040
周明皓 <847710682@qq.com>
2016-11-16 17:40:25 -0800

    初始化项目

---
+++ 美团2/美团2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  美团2
+//
+//  Created by mac on 16/11/16.
+//  Copyright © 2016年 macd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 美团2/美团2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  美团2
+//
+//  Created by mac on 16/11/16.
+//  Copyright © 2016年 macd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
